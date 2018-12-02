183eec11dfe8e471290775ee319b24c4cd67907c
Tatiana Zhmaylo <tanyshikzh@yandex.ru>
2018-12-02 19:32:33 +0300

    Spring

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -5,24 +5,32 @@ import ru.sbt.mipt.oop.Alarm.AlarmEventProcessor;
 import ru.sbt.mipt.oop.Events.SensorEventManagerAdapter;
 import ru.sbt.mipt.oop.Events.SensorEventsManager;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 
 public class Application {
 
+    private static Logger logger = LogManager.getLogger(Application.class);
     private static SmartHomeLoader smartHomeLoader = new FileSmartHomeLoader();
-
-
+    private static EventsManager eventsManager = new EventsManagerAdapter(new SensorEventsManager());
 
     public static void setSmartHomeLoader(SmartHomeLoader smartHomeLoader) {
         Application.smartHomeLoader = smartHomeLoader;
     }
 
     public static void main(String... args) throws IOException {
+        logger.info("Starting configuration...");
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        SmartHomeLoader smartHomeLoader = context.getBean(SmartHomeLoader.class);
+        EventsManager eventsManager = context.getBean(EventsManager.class);
+
         SensorEventProvider sensorEventProvider = new RandomSensorEventProvider();
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
         EventsManager eventManager = new SensorEventManagerAdapter(sensorEventsManager);
-
-        SmartHome smartHome = smartHomeLoader.loadSmartHome();
 
         eventManager.registerEventProcessor(new LightsEventProcessor());
         eventManager.registerEventProcessor(new DoorEventProcessor());
